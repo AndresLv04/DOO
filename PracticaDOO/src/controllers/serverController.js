f220f175aa2d7fabea6a124647a472092cde7016
Andres Lopez <166462520+AndresLv04@users.noreply.github.com>
2025-05-22 20:01:34 +0000

    Agregue los metodos que faltaban

--- PracticaDOO/src/controllers/serverController.js
+++ PracticaDOO/src/controllers/serverController.js
@@ -7,7 +7,7 @@ class ServerController {
     //Obtiene todos los registros de la colección de la base de datos NoSQL
     async getAllUsers(req, res) {
         try {
-            const data = await person.find();  // Await the promise returned by .find()
+            const data = await person.find();
             res.status(200).json(data);
         } catch (error) {
             res.status(500).send({ error: "Error retrieving users from the database" });
@@ -16,16 +16,54 @@ class ServerController {
 
 
     //Obtiene el registro según el id que le estemos pasando a la colección de la base de datos NoSQL
-    getUsers(req, res) {
+    async getUsers(req, res) {
         let id = req.params.id;
-        person.findById(id, (error, data) => {
-            if (error) {
-                res.status(500).send();
-            } else {
-                res.status(200).json(data);
-            }
-        })
+        try {
+            let data = await person.findById(id);
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).send();
+        }
     }
+
+    async register (req, res){
+        const nuevoUsuario = new person(req.body);
+        const guardarUsario = await nuevoUsuario.save();
+        if(guardarUsario){
+            res.status(201).json(guardarUsario);
+        }else{
+            res.status(500).json({error: 'Error al guardar el usuario'});
+        }
+    }
+
+    async update(req, res){
+        let id = req.params.id;
+        let datos = req.body;
+
+        let usuarioActualizar = await person.findByIdAndUpdate(id, datos, {new: true});
+
+        if(usuarioActualizar){
+            res.status(200).json({ message: "Usuario actualizado correctamente" });
+        }else{
+            res.status(404).json({ message: "Usuario no encontrado" });
+        }
+    }
+
+    //Elimina a un registro según el id que le pasemos  a la colección de BD NoSQL
+    async deleteUser(req, res){
+        let id = req.params.id
+
+        let deleteUser = await person.findByIdAndDelete(id);
+
+        if(deleteUser){
+            res.status(200).json({message: "Usuario con id: " + id + " fue eliminado correctamente"});
+        }else{
+            res.status(404).json({message: "Usuario con id: " + id + " no fue encontrado"});
+        }
+
+    }
+
+    
 
 }
 
--- PracticaDOO/src/routers/serverRouter.js
+++ PracticaDOO/src/routers/serverRouter.js
@@ -9,11 +9,12 @@ class ServerRouter{
 
     config(){
         const objServerC = new serverController.default();
+        this.router.delete("/users/:id", objServerC.deleteUser); 
         this.router.get("/users", objServerC.getAllUsers);
         this.router.get("/users/:id", objServerC.getUsers);
-        //this.router.post("/users", objServerC.register);
-        //this.router.put("/users", objServerC.update);
-        //this.router.delete("/users", objServerC.deleteUser);        
+        this.router.post("/users", objServerC.register);
+        this.router.put("/users/:id", objServerC.update);
+               
     }
 }
 
